2b5f29170045b17a218f7944c2cd030057629633
Ivanbalderascarmona <balderascarmonaivan@gmail.com>
2025-10-20 13:05:54 +0200

    Updated userController.php, FileHelper.php and UserRepository.php

--- controllers/mainController.php
+++ controllers/mainController.php
@@ -2,3 +2,20 @@
 
 require_once("models/User.php");
 require_once("models/CartItems.php");
+require_once("models/Cart.php");
+require_once("models/Orders.php");
+require_once("models/Products.php");
+
+session_start();
+
+$db = Connection::connect();
+
+if (!isset($_SESSION['user'])) {
+    $_SESSION['user'] = false;
+}
+
+if(isset($_GET['c'])){
+    require_once('controllers/'.$_GET['c'].'Controller.php');
+}else {
+    require_once('controllers/userController.php');
+}
---
+++ models/UserRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+class UserRepository{
+    public static function login($username, $password){
+        $db = Connection::connect();
+        $q = 'SELECT * FROM users WHERE username="'.$username.'" AND password="'.md5($password).'"';
+        $result = $db->query($q);
+        if($row = $result->fetch_assoc()){
+            return new User($row['id'],$row['username'],$row['role'], $row['avatar']);
+        }else{
+            return false;
+        }
+    }
+
+    public static function register($username, $password, $email, $avatar){
+        $db = Connection::connect();
+        //# Comprobar que el usuario no existe
+        $q = 'SELECT * FROM users WHERE username="'.$username.'"';
+        $result = $db->query($q);
+        if($result->num_rows > 0){
+            require_once('views/registerView.phtml');
+            exit();
+        }
+        $q = 'INSERT INTO users (username, password, email, role, avatar) VALUES ("'.$username.'", "'.md5($password).'" ,"'.$email.'", 0, "' . $avatar . '")';
+        $db->query($q);
+        if($db->insert_id){
+            $success = "Usuario registrado correctamente. Ya puedes iniciar sesión.";
+            require_once('views/loginView.phtml');
+        }else{
+            $error = "Error al registrar el usuario";
+            require_once('views/registerView.phtml');
+        }
+        exit();
+    }
+}
